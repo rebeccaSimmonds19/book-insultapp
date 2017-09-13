e7c6c8510d3f993ee6d665512a7a7076417bb927
Rebecca Simmonds <rsimmond@rebeccasimmonds-OSX.local>
2017-09-13 10:45:33 +0100

    changed for the db

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,19 +1,46 @@
 package org.openshift;
 
 import java.util.Random;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class InsultGenerator {
-	public String generateInsult() {
+	public String generateInsult()
+	{
+
 		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
 		String vowels = "AEIOU";
 		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
-		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
-	}
+		String theInsult = "";
+		try {
+			String databaseUrl = "jdbc:postgreswl://";
+			databaseUrl += System.getenv("POSTGRESQL_SERVICE_HOST");
+			databaseUrl += System.getenv("POSTGRESQL_DATABASE");
+			String username = System.getenv("POSTGRESQL_USER");
+			String password = System.getenv("PGPASSWORD");
 
+			Connection connection = DriverManager.getConnection(databaseUrl, username, password);
+
+			if (connection != null) {
+				String sql = "select a.string AS first , b.string AS second, c.string AS noun from short_adjective a, long_adjective b, noun c ORDER BY random() limit 1";
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(sql);
+				while (resultSet.next()) {
+					if (vowels.indexOf(resultSet.getString("first").charAt(0)) == -1) {
+						article = "a";
+					}
+					theInsult = String.format("Thou art %s %s %s %s!", article, resultSet.getString("first"), resultSet.getString("second"), resultSet.getString("noun"));
+				}
+				resultSet.close();
+				connection.close();
+			}
+
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+		return theInsult;
+	}
 }
